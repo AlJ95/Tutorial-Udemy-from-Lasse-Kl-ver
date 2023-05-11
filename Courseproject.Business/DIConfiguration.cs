e32ff5ddee343ea8fde7c56bb3802a7950fc6fc9
AlJ95 <jan.albrecht1995@gmail.com>
2023-05-12 00:02:38 +0200

    Projektdateien hinzufügen.

---
+++ Courseproject.Business/DIConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Courseproject.Business.Services;
+using Courseproject.Common.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Courseproject.Business;
+
+public class DIConfiguration
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(DtoEntityMapperProfile));
+        services.AddScoped<IAddressService, AddressService>();
+        services.AddScoped<IJobService, JobService>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<ITeamService, TeamService>();
+    }
+}
